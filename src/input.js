c890238b29643ad781f357021ca05fb0605c058c
Frank Scalise <ashtonscalise@gmail.com>
2020-03-25 11:06:04 -0500

    added button to get location, still need to handle form validation

--- src/input.js
+++ src/input.js
@@ -1,14 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Spinner from "react-bootstrap/Spinner";
 import axios from "axios";
+import { geolocated } from "react-geolocated";
 
 const Input = () => {
   const [state, updateState] = useState("77406");
   const [restaurant, updateRestaurant] = useState();
   const [imageUrl, updateImageUrl] = useState();
   const [yelpUrl, updateYelpUrl] = useState();
+  const [latitude, updateLatitude] = useState();
+  const [longitude, updateLongitude] = useState();
 
   const [isLoading, updateIsLoading] = useState(false);
   const url = process.env.REACT_APP_API_URL;
+
+  const size = useWindowSize();
+
+  const buttonDesktop = { width: "224px" };
+  const buttonMobile = { width: "100%" };
 
   const handleChange = async () => {
     updateRestaurant("");
@@ -29,27 +40,96 @@ const Input = () => {
         updateIsLoading(false);
       });
   };
+
+  function useWindowSize() {
+    const isClient = typeof window === "object";
+
+    function getSize() {
+      return {
+        width: isClient ? window.innerWidth : undefined,
+        height: isClient ? window.innerHeight : undefined
+      };
+    }
+
+    const [windowSize, setWindowSize] = useState(getSize);
+
+    useEffect(() => {
+      if (!isClient) {
+        return false;
+      }
+
+      function handleResize() {
+        setWindowSize(getSize());
+      }
+
+      window.addEventListener("resize", handleResize);
+      return () => window.removeEventListener("resize", handleResize);
+    }, []); // Empty array ensures that effect is only run on mount and unmount
+
+    return windowSize;
+  }
+
+  async function position() {
+    var options = {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0
+    };
+
+    function success(pos) {
+      var crd = pos.coords;
+
+      console.log("Your current position is:");
+      console.log(`Latitude : ${crd.latitude}`);
+      console.log(`Longitude: ${crd.longitude}`);
+      console.log(`More or less ${crd.accuracy} meters.`);
+
+      updateLatitude(crd.latitude);
+      updateLongitude(crd.longitude);
+      console.log(latitude);
+      updateState(crd.latitude + "," + crd.longitude);
+    }
+
+    function error(err) {
+      console.warn(`ERROR(${err.code}): ${err.message}`);
+    }
+
+    navigator.geolocation.getCurrentPosition(success, error, options);
+  }
+
   return (
     <div>
-      <h1>Enter zip code</h1>
-      <form action="">
-        <div className="form-group">
-          <label htmlFor=""></label>
-          <input
-            type="text"
-            className="form-control"
-            value={state}
-            onChange={e => updateState(e.target.value)}
-          />
-        </div>
-      </form>
-      <br></br>
+      <Form.Label>Enter zipcode</Form.Label>
+      <Form.Control
+        style={{ width: "100%" }}
+        variant="primary"
+        type="text"
+        value={state}
+        onChange={e => updateState(e.target.value)}
+      />
       {!isLoading ? (
-        <button className="button" onClick={handleChange}>
+        <Button
+          onClick={handleChange}
+          variant="primary"
+          style={size.width < 768 ? buttonMobile : buttonDesktop}
+        >
           Search
-        </button>
+        </Button>
       ) : (
-        <div className="loader"></div>
+        <Button
+          variant="primary"
+          disabled
+          style={size.width < 768 ? buttonMobile : buttonDesktop}
+        >
+          <Spinner
+            as="span"
+            animation="border"
+            size="sm"
+            role="status"
+            aria-hidden="true"
+          />
+          <span className="sr-only">Loading...</span>
+        </Button>
       )}
       <div>
         <h1>{restaurant}</h1>
@@ -67,7 +147,14 @@ const Input = () => {
           ""
         )}
       </div>
-      <div style={{ maxWidth: "800px" }}>
+      <Button
+        onClick={position}
+        variant="primary"
+        style={size.width < 768 ? buttonMobile : buttonDesktop}
+      >
+        Get my Location (WIP)
+      </Button>
+      {/* <div style={{ maxWidth: "800px" }}>
         <h6>
           Support this App by using this link for $5 off your first three
           doordash orders
@@ -79,7 +166,7 @@ const Input = () => {
             style={({ height: "200px" }, { width: "100%" })}
           />
         </a>
-      </div>
+      </div> */}
     </div>
   );
 };
